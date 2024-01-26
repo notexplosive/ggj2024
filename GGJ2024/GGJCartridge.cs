828b43116a70739d525a4b8aa4c1e7186e5ad194
notexplosive <thenotexplosive@gmail.com>
2024-01-26 10:51:57 -0800

    Repo setup, awaiting theme announcement, happy jamming

---
+++ GGJ2024/GGJCartridge.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using ExplogineCore;
+using ExplogineMonoGame;
+using ExplogineMonoGame.Cartridges;
+using ExplogineMonoGame.Data;
+using Microsoft.Xna.Framework;
+
+namespace GGJ2024;
+
+public class GGJCartridge : BasicGameCartridge
+{
+    public GGJCartridge(IRuntime runtime) : base(runtime)
+    {
+    }
+
+    public override CartridgeConfig CartridgeConfig { get; } = new(new Point(1920, 1080));
+    public override void OnCartridgeStarted()
+    {
+        Client.Debug.Log("Awakened");
+    }
+
+    public override void UpdateInput(ConsumableInput input, HitTestStack hitTestStack)
+    {
+        
+    }
+
+    public override void Update(float dt)
+    {
+        
+    }
+
+    public override void Draw(Painter painter)
+    {
+        
+    }
+
+    public override void AddCommandLineParameters(CommandLineParametersWriter parameters)
+    {
+        
+    }
+
+    public override IEnumerable<ILoadEvent> LoadEvents(Painter painter)
+    {
+        Client.Debug.Log("Load");
+        yield break;
+    }
+
+    public override void Unload()
+    {
+    }
+
+    public override void OnHotReload()
+    {
+    }
+}
